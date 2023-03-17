f6eebe999c7ae5bf31ad6246888165eda18b64e8
mpw <x@mpw.sh>
2023-03-17 15:44:51 -0300

    add config builder, add chiactl example

---
+++ examples/chiactl.rs
@@ -0,0 +1,122 @@
+use anyhow::Result;
+use chia_node::fullnode::Client as FullNodeClient;
+use chia_node::fullnode::Config as FullNodeConfig;
+use chia_node::util::decode_puzzle_hash;
+use serde::Deserialize;
+use std::fs;
+use std::net::SocketAddr;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = "chiactl",
+    about = "A CLI to interact with Chia RPC API Endpoints"
+)]
+pub struct Cli {
+    #[structopt(flatten)]
+    global: GlobalOptions,
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct GlobalOptions {
+    #[structopt(short, long = "key-path", global = true, help = "Path to FullNode Key", parse(from_os_str))]
+    key_path: Option<PathBuf>,
+
+    #[structopt(short, long = "cert-path", global = true, help = "Path to FullNode Cert", parse(from_os_str))]
+    cert_path: Option<PathBuf>,
+
+    #[structopt(long, global = true, help = "IP of Chia Node")]
+    host: Option<String>,
+
+    #[structopt(long, global = true, help = "Port of Chia FullNode RPC")]
+    port: Option<u16>,
+
+    #[structopt(long, global = true, help = "config path for chiactl", parse(from_os_str))]
+    config: Option<PathBuf>,
+}
+
+#[derive(Debug, Deserialize)]
+struct ConfigFile {
+    host: String,
+    port: u16,
+    key_path: PathBuf,
+    cert_path: PathBuf,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum Command {
+    #[structopt(name = "get")]
+    Get {
+        #[structopt(subcommand)]
+        subcommand: GetSubcommand,
+    },
+}
+
+#[derive(Debug, StructOpt)]
+pub enum GetSubcommand {
+    #[structopt(name = "balance")]
+    Balance {
+        #[structopt(name = "wallet_address")]
+        address: String,
+    },
+}
+
+impl Cli {
+    pub async fn load_config(&self) -> Result<FullNodeConfig> {
+        // Read the config file and parse it as YAML
+        let config_path = if let Some(ref config) = self.global.config {
+            config.clone()
+        } else {
+            dirs::home_dir().unwrap().join(".chiactl/config.yaml")
+        };
+        let config_str = fs::read_to_string(config_path)?;
+        let config_file: ConfigFile = serde_yaml::from_str(&config_str)?;
+
+        // Use the values from the config file as the default values and override them with the values
+        // provided as arguments if available
+        let host = self.global.host.clone().unwrap_or(config_file.host);
+        let port = self.global.port.unwrap_or(config_file.port);
+        let key_path = self.global.key_path.clone().unwrap_or(config_file.key_path);
+        let cert_path = self
+            .global
+            .cert_path
+            .clone()
+            .unwrap_or(config_file.cert_path);
+
+        Ok(FullNodeConfig::new(
+            SocketAddr::new(host.parse()?, port),
+            &key_path,
+            &cert_path,
+        ))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::from_args();
+    let config = cli.load_config();
+    let client = FullNodeClient::new(config.await?).await?;
+    match cli.cmd {
+        Command::Get { subcommand } => {
+            match subcommand {
+                GetSubcommand::Balance { address } => get_balance(&client, address).await?,
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn get_balance(client: &FullNodeClient, address: String) -> Result<()> {
+    let puzzle_hash = decode_puzzle_hash(&address)?;
+    let response = client
+        .get_coin_records_by_puzzle_hash(&puzzle_hash, None, None, Some(false))
+        .await?;
+    let balance_mojos: u64 = response.iter().map(|record| record.coin.amount).sum();
+    let balance_xch: f64 = balance_mojos as f64 / 1_000_000_000_000.0;
+    println!("Balance: {:.12} XCH", balance_xch);
+    Ok(())
+}
--- examples/get_balance.rs
+++ examples/get_balance.rs
@@ -1,19 +1,31 @@
 use anyhow::Result;
 use chia_node::fullnode::Client as FullNodeClient;
+use chia_node::fullnode::ConfigBuilder as FullNodeConfigBuilder;
 use chia_node::util::decode_puzzle_hash;
+use std::net::{Ipv4Addr, SocketAddr};
 use std::path::Path;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     //Configure your node
-    let host = "192.168.1.31";
+    let ip: Ipv4Addr = "192.168.1.31".parse().expect("Invalid IPv4 address");
     let port: u16 = 8555;
-    let key_path = Path::new("<path-to-fullnode-key>");
-    let cert_path = Path::new("<path-to-fullnode-cert>");
-    let client = FullNodeClient::new(host, port, key_path, cert_path).await?;
+    let addr = SocketAddr::new(ip.into(), port);
+    let key_path = Path::new("/Users/mpw/projects/chia/certs/private_full_node.key");
+    let cert_path = Path::new("/Users/mpw/projects/chia/certs/private_full_node.crt");
+
+    let config = FullNodeConfigBuilder::new()
+        .addr(addr)
+        .key_path(key_path)
+        .cert_path(cert_path)
+        .build()
+        .expect("Failed to create FullNodeConfig");
+
+    let client = FullNodeClient::new(config).await?;
 
     //Insert your wallet address
-    let puzzle_hash = decode_puzzle_hash("<your-wallet-address>")?;
+    let puzzle_hash =
+        decode_puzzle_hash("xch10n6l66hhx3qrx2ttdvaj54mmy2u63jvhzalj5t6d89npsl4psmvqtsq8fz")?;
     let response = client
         .get_coin_records_by_puzzle_hash(&puzzle_hash, None, None, Some(false))
         .await?;
