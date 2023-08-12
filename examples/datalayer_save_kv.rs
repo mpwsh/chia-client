67dbd47278d665fbe0bcc031c1a413404328fa31
mpw <x@mpw.sh>
2023-08-11 22:59:09 -0300

    update examples

--- examples/datalayer_save_kv.rs
+++ examples/datalayer_save_kv.rs
@@ -59,37 +59,35 @@ async fn main() -> Result<()> {
 ||||||| parent of a2d8e39 (introduce datalayer api, migrate models and rename crate to chia-client)
 =======
 use anyhow::{anyhow, Result};
-use chia_client::{
-    datalayer::{Client, ConfigBuilder},
-    util::xch_to_mojo,
-};
-use std::{
-    net::{Ipv4Addr, SocketAddr},
-    path::Path,
-};
+use chia_client::{datalayer, util::xch_to_mojo, ClientBuilder};
+use std::path::Path;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    //Configure your node
-    let ip: Ipv4Addr = "192.168.2.74".parse().expect("Invalid IPv4 address");
-    let port: u16 = 8562;
-    let addr = SocketAddr::new(ip.into(), port);
+    //Use the full path for your certs.
     let key_path =
         Path::new("/Users/mpw/projects/chia/chia-node-rs/data_layer/private_data_layer.key");
     let cert_path =
         Path::new("/Users/mpw/projects/chia/chia-node-rs/data_layer/private_data_layer.crt");
 
-    let config = ConfigBuilder::new()
-        .addr(addr)
+    let client = ClientBuilder::new()
+        .addr("192.168.2.74", 8562)
         .key_path(key_path)
         .cert_path(cert_path)
         .build()
-        .expect("Failed to create Config");
+        .await?;
 
-    let client = Client::new(config).await?;
+    let client = datalayer::Rpc::init(client);
     let fee = 0.00005;
 
-    //let res = client.create_data_store(xch_to_mojo(fee)).await?;
+    let tx = match client.create_data_store(xch_to_mojo(fee)).await {
+        Ok(tx) => tx,
+        Err(e) => {
+            println!("Error: {:?}", e);
+            return Err(anyhow!("Error: {:?}", e));
+        }
+    };
+    println!("Store created - Tx details: {:?}", tx);
 
     let store_id = client.get_owned_stores().await?[0].clone();
     println!("{store_id}");
@@ -97,11 +95,9 @@ async fn main() -> Result<()> {
     //Insert a Key/value on a specific store_id
     let key = "0x0001";
     let value = "fadedcab";
-    /*
-        let insert_tx_id = client.insert(&store_id, key, value).await?;
+    let insert_tx_id = client.insert(&store_id, key, value).await?;
 
-        println!("{:?}", insert_tx_id);
-    */
+    println!("{:?}", insert_tx_id);
     //Retrieve it
     let root_hash = match client.get_root(&store_id).await?.hash {
         Some(hash) => hash,
@@ -110,7 +106,6 @@ async fn main() -> Result<()> {
 
     println!("Using root hash: {root_hash}");
 
-    //returns key not found after few days (wtf)
     let retrieved_value = client.get_value(&store_id, key, &root_hash).await?;
     println!("{retrieved_value}");
 
